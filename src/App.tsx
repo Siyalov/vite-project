6e2fd2219f35e12af694b67c75debb26b836bd64
Ilya Siyalov <ilya.siyalov@gmail.com>
2024-11-16 20:07:40 +0100

    moving box

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
+
 import './App.css'
 
+const SIZE = 34;
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  useEffect( () => {
+    window.addEventListener('mousemove', (event)=> {
+      setX(event.x)
+      setY(event.y)
+    })
+  },[])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     {x},
+     {y} 
+      <div style={{position:'absolute', left: x-SIZE/2, top: y-SIZE/2, width: SIZE, height: SIZE, backgroundColor: 'black'}}>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
